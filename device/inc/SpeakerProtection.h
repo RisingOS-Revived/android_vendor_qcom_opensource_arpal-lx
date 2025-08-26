74d08686d4208cf2736c5a2fe0ccbb00dcb8249a
keosh <conquer2152016@gmail.com>
2025-08-26 00:46:22 +0000

    Revert "pal: SpeakerProtectionTfa98xx: Initial implementation"
    This reverts commit 10e299ce77332abae951408b5c7af3dbddffc4db.

--- device/inc/SpeakerProtection.h
+++ device/inc/SpeakerProtection.h
@@ -41,7 +41,6 @@
 #include<vector>
 #include "apm_api.h"
 #include "ResourceManager.h"
-#include "SpeakerProtectionTfa98xx.h"
 
 class Device;
 
@@ -116,7 +115,6 @@ protected :
     struct spDeviceInfo spDevInfo;
     void *viCustomPayload;
     size_t viCustomPayloadSize;
-    std::unique_ptr<SpeakerProtectionTfa98xx> tfa98xx;
 
 private :
     static bool isSharedBE;
